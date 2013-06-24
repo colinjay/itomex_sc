fd2334c8463e67ed2e66d2a04d6136cec7855647
Ramiro Rivera <rami.river@gmail.com>
2013-06-24 19:34:14 +0200

    Links and footer added

--- themes/cosmo/views/site/index.php
+++ themes/cosmo/views/site/index.php
@@ -3,31 +3,6 @@
 
 $this->pageTitle=Yii::app()->name;
 ?>
-<div class="clear"></div>
-<div class="row-fluid">
-	<div class="span4">
-		<img src="/itomex_sc/images/Logo_WWU_Münster.svg" class="logo" />
-	</div>
-	<div class="span4 offset4">
-		<img src="/itomex_sc/images/itomex_logo.png" class="logo2" />
-	</div>
-</div>
-<div class="clear"></div>
-<div class="row-fluid">
-	<div class="navbar">
-		<div class="navbar-inner">
-			<a class="brand" href="#">ITOMEX Service Catalogue</a>
-			<div class="nav-collapse collapse">
-				<ul class="nav">
-					<li class="active"><a href="#">Home</a></li>
-					<li><a href="#about">About</a></li>
-					<li><a href="#contact">Contact</a></li>
-					<li><a href="#contact">Login</a></li>
-				</ul>
-			</div>
-		</div>
-	</div>
-</div>
 <div class="row-fluid">
 	<h3>All information is directly reported to the NSA!</h3>
 	<hr />
@@ -36,31 +11,43 @@ $this->pageTitle=Yii::app()->name;
 	<div class="span9">
 		<div class="row-fluid">
 			<div class="span4 box1">
-				<h4>Email & Messaging</h4>
-				<img alt="Mail" src="/itomex_sc/images/mail.svg" />
+				<a href="#">
+					<h4>Email & Messaging</h4>
+					<img alt="Mail" src="/itomex_sc/images/mail.svg" />
+				</a>
 			</div>
 			<div class="span4 box2">
-				<h4>Network</h4>
-				<img alt="Network" src="/itomex_sc/images/network.svg" />
+				<a href="#">
+					<h4>Network</h4>
+					<img alt="Network" src="/itomex_sc/images/network.svg" />
+				</a>
 			</div>
 			<div class="span4 box3">
-				<h4>Machine Care</h4>
-				<img alt="Machine Care" src="/itomex_sc/images/pda.svg" />
+				<a href="#">
+					<h4>Machine Care</h4>
+					<img alt="Machine Care" src="/itomex_sc/images/pda.svg" />
+				</a>
 			</div>
 		</div>
 		<div class="clear"></div>
 		<div class="row-fluid">
 			<div class="span4 box4">
-				<h4>Collaboration</h4>
-				<img alt="Collaboration" src="/itomex_sc/images/group.svg" />
+				<a href="#">
+					<h4>Collaboration</h4>
+					<img alt="Collaboration" src="/itomex_sc/images/group.svg" />
+				</a>
 			</div>
 			<div class="span4 box5">
-				<h4>Desktop Services</h4>
-				<img alt="Desktop Services" src="/itomex_sc/images/computer.svg" />
+				<a href="#">
+					<h4>Desktop Services</h4>
+					<img alt="Desktop Services" src="/itomex_sc/images/computer.svg" />
+				</a>
 			</div>
 			<div class="span4 box6">
-				<h4>Computational Performance</h4>
-				<img alt="Computational Performanc" src="/itomex_sc/images/server.svg" />
+				<a href="#">
+					<h4>Computational Performance</h4>
+					<img alt="Computational Performanc" src="/itomex_sc/images/server.svg" />
+				</a>
 			</div>
 		</div>
 	</div>
